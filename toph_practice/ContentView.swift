76fff5a785a8974443b146213479a845061a8a9a
Topher Jacobus <topher.jacobus@gmail.com>
2022-12-25 20:41:32 -0600

    Initial Commit

---
+++ toph_practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  toph_practice
+//
+//  Created by topher jacobus on 12/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ toph_practice/toph_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  toph_practiceApp.swift
+//  toph_practice
+//
+//  Created by topher jacobus on 12/25/22.
+//
+
+import SwiftUI
+
+@main
+struct toph_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
