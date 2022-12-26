20a7fc7aa687d85bbb5d4f349cde88d656e467e7
Topher Jacobus <topher.jacobus@gmail.com>
2022-12-26 13:17:29 -0600

    updated code

--- toph_practice/ContentView.swift
+++ toph_practice/ContentView.swift
@@ -9,10 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            DisclosureGroup("SAY HELLO") {
+                Text("Hello, world!")
+                    .padding()
+            }
+            .padding(25)
+            
+            Text("Hello, world NEW!")
+                .padding()
+            
+            LinearGradient(gradient: /*@START_MENU_TOKEN@*/Gradient(colors: [Color.red, Color.blue])/*@END_MENU_TOKEN@*/, startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/)
+            }
+        
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
